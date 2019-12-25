0cfff71b2ef2174115b566b09ddf32c6e0e3d682
Steffi Motzokan <stefanie.motzokan@maibornwolff.de>
2019-12-25 21:57:49 +0100

    changing some infos

--- webpage/src/App.vue
+++ webpage/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="humanIT" width="30%" src="./assets/hands.png">
+    <HelloWorld msg="Welcome to ShakeIT!"/>
   </div>
 </template>
 
