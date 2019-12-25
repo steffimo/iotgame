d8547d1e74f5f62f38cd29cda165d01c2e83857c
Steffi Motzokan <stefanie.motzokan@maibornwolff.de>
2019-12-26 00:05:37 +0100

    "Go for IT"-Button with some example handling function

--- webpage/src/components/HelloWorld.vue
+++ webpage/src/components/HelloWorld.vue
@@ -2,8 +2,17 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      ShakeIT is a simple and fun Internet of Things game, used as an IoT showcase in purpose of a thesis.
+      ShakeIT is a simple and fun Internet of Things game, used for the development of an IoT showcase in purpose of a thesis.
     </p>
+    <p>
+    So now register to the next session and create your username. After pressing the button "Go for IT" shake your device for 10 seconds.
+    </p>
+    <div id="StartButton">
+      <button @click="startDataTransfer">Go for IT</button>
+      <div class="xValue">
+        <p>shakedX {{xValue}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,9 +20,18 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+  },
+  data: function() {
+    return {xValue: "to shake"};
+  },
+  methods: {
+    startDataTransfer() {
+      this.xValue = "Test";
+    }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
