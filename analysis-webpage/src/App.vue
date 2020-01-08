147c03a7237995afd0385ea41b6d1e2caeb02a05
Steffi Motzokan <stefanie.motzokan@maibornwolff.de>
2020-01-08 17:48:38 +0100

    analysis webpage updated

--- analysis-webpage/src/App.vue
+++ analysis-webpage/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Analytics logo" src="./assets/analytics.png">
+    <HelloWorld msg="Welcome to Your Analytics"/>
   </div>
 </template>
 
